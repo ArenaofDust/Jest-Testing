79ea1183b301a7bdb6ff3b2ecf8e68411552659f
ArenaofDust <149437984+ArenaofDust@users.noreply.github.com>
2024-07-11 18:24:58 -0400

    Created tests for calculator objects

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-const { capitalize, reverseString } = require('./functions');
+const { capitalize, reverseString, calculator } = require('./functions');
 
 describe('capitalize', () => {
     test('Capitalizes first letter in string', () => {
@@ -10,4 +10,23 @@ describe('reverseString', () => {
     test('Reverses a given string', () => {
     expect(reverseString('hello')).toBe('olleh');
     });
+});
+
+describe('calculator', () => {
+    test('Tests add function of calculator object', () => {
+        expect(calculator.add(1, 2)).toBe(3);
+    })
+
+    test('Tests subtract function of calculator object', () => {
+        expect(calculator.subtract(4, 1)).toBe(3);
+    })
+
+    test('Tests divide function of calculator object', () => {
+        expect(calculator.divide(9, 3)).toBe(3);
+    })
+
+    test('Tests multiply function of calculator object', () => {
+        expect(calculator.multiply(3, 3)).toBe(9);
+    })
+
 });
