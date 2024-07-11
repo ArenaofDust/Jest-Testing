f5a53e30658bd1011755f3d94259296c871d7420
ArenaofDust <149437984+ArenaofDust@users.noreply.github.com>
2024-07-11 18:06:51 -0400

    Added reverseString function

--- functions.js
+++ functions.js
@@ -1,3 +1,16 @@
-function capitalize(userString) {
-    return userString.charAr(0).toUpperCase() + String.slice(1);
+function capitalize(str) {
+    if (!str) return '';
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+
+function reverseString(str) {
+    if(!str) return '';
+
+    return str.split("").reverse().join("");
+}
+
+module.exports = {
+    capitalize,
+    reverseString
+};
